5522d4496c06dacac9bc2ce51935c6ffccafc6be
uu-marina <idid02@naver.com>
2023-04-10 18:28:51 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h4> HELLO WORLD !</h4> <h2>MY NAME ==  MIN JIN ^_^ </h2> 
+            <Link href="/">Back to home</Link>
+        </div>
+    )
+}
